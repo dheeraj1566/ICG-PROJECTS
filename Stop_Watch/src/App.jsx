7b452688cae3ae9965fb6f6bf328d3c88b824dc9
dheeraj1566 <dheerajjangid013@gmail.com>
2025-01-09 22:48:23 +0530

    Expance_tracker and Movie_search are not working

---
+++ Stop_Watch/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+// import StopWatch from './Components/StopWatch/StopWatch.js';
+import StopWatch from '../components/StopWatch';
+
+function App() {
+	return (
+		<div className="App">
+			<StopWatch />
+		</div>
+	);
+}
+
+export default App;
