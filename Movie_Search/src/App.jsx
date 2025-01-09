121a4ba3df0690a853bcf6d697e5e221d7f8806d
dheeraj1566 <dheerajjangid013@gmail.com>
2025-01-09 22:58:25 +0530

    movie_sreach complete

--- Movie_Search/src/App.jsx
+++ Movie_Search/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App..js
+
+import './App.css';
+import { useState } from 'react';
+import ReactPlayer from 'react-player';
+import movieTrailer from 'movie-trailer';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    //Setting up the initial states using
+    // react hook 'useState"
+    const [video, setVideo] = useState("inception");
+    const [videoURL, setVideoURL] =
+        useState("https://youtu.be/sa9l-dTv9Gk");
+
+    //A function to fetch the required URL
+    // and storing it inside the
+    // videoURL state variable
+    function handleSearch() {
+        movieTrailer(video).then((res) => {
+            setVideoURL(res);
+        });
+    }
+
+    return (
+        <div className="App">
+            <div className="search-box">
+                <label>
+                    Search for any movies/shows:{" "}
+                </label>
+                <input type="text" onChange=
+                    {(e) => { setVideo(e.target.value) }} />
+
+                <button onClick={() => { handleSearch() }}>
+                    Search
+                </button>
+            </div>
+
+      // Using 'ReactPlayer' component to
+            // display the video
+            <ReactPlayer url={videoURL} controls={true} />
+        </div>
+    );
 }
 
-export default App
+export default App;
